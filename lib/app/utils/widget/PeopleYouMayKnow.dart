6d3c43586c360b9137e583272d8397ee8d92de3a
WahyuKurniaSenjaya <87650735+WahyuKurniaSenjaya@users.noreply.github.com>
2022-12-23 22:48:40 +0700

    commit 8

---
+++ lib/app/utils/widget/PeopleYouMayKnow.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
+
+import '../../data/controller/auth_controller.dart';
+
+class PeopleYouMayKnow extends StatelessWidget {
+  final authCon = Get.find<AuthController>();
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: FutureBuilder(
+          future: authCon.getPeople(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            }
+
+            var data = snapshot.data!.docs;
+
+            return ListView.builder(
+              scrollDirection: Axis.horizontal,
+              shrinkWrap: true,
+              clipBehavior: Clip.antiAlias,
+              itemCount: data.length,
+              itemBuilder: (context, index) {
+                var hasil = data[index].data();
+                return Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Stack(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(30),
+                        child: Image(
+                          image: NetworkImage(hasil['photo']),
+                          height: 200,
+                          width: 200,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Positioned(
+                        bottom: 10,
+                        left: 10,
+                        child: Text(
+                          hasil['name'],
+                          style: const TextStyle(
+                              color: Colors.white, fontSize: 15),
+                        ),
+                      ),
+                      Positioned(
+                        bottom: 0,
+                        right: 0,
+                        child: SizedBox(
+                          height: 36,
+                          width: 36,
+                          child: ElevatedButton(
+                            onPressed: () => authCon.addFriends(hasil['email']),
+                            style: ElevatedButton.styleFrom(
+                              padding: EdgeInsets.zero,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(40),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            );
+          }),
+    );
+  }
+}
